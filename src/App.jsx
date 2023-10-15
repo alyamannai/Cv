19eee0cfce48acb8f605a6b0278d125441584230
alyamannai <alyamannaimat@gmail.com>
2023-10-15 13:43:52 +0400

    created input field that shows real time info and can submit and save it + a clear button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [jobD, setJobD] = useState('');
+  const [jobT, setJobT] = useState('');
+  const [displayedText, setDisplayedText] = useState('');
+  const [isFormVisible, setFormVisible] = useState(false);
 
+  const handleToggleForm = () => {
+    setFormVisible(!isFormVisible);
+  };
+
+  const handleChange = (e) => {
+    setJobD(e.target.value);
+    setDisplayedText(e.target.value)
+  };
+
+  const saveTheText = (e) => {
+    e.preventDefault();
+    setDisplayedText(jobD);
+    setJobD('');
+    setFormVisible(false);
+  };
+
+  const clearForm = () => {
+    setDisplayedText('');
+  };
+
+  return (
+    <div className="container">
+      <a className="toggle-button" onClick={handleToggleForm}>
+        {isFormVisible ? 'Job Description' : 'Job Description'}
+      </a>
+      {isFormVisible && (
+        <form className="form">
+          <input
+            value={jobD}
+            onChange={handleChange}
+            placeholder="Enter job description"
+          />
+         
+          <button className="clear-button" onClick={clearForm}>
+            Clear
+          </button>
+          <button className="save-button" type="submit" onClick={saveTheText}>
+            Save
+          </button>
+        </form>
+        
+      )}
+       <p className="displayed-text">{displayedText}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
